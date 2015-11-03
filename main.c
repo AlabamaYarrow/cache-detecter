63c1bfbe84afc0d1e4f410cc6f47d918530d4b77
Ivan Dunin <johnmoony@mail.ru>
2015-11-03 20:39:38 +0200

    Random iteration.

--- main.c
+++ main.c
@@ -9,20 +9,20 @@
 #define TOP_SIZE 70000000 
 #define PAYLOAD_SIZE 7
 #define ITERATIONS 5000000
-
-void print_cpu_info(FILE* fp);
-void do_sequential_iteration(FILE* fp);
-void do_random_iteration(FILE* fp);
+#define ITERATION_CYCLES 30
 
 struct Node {
 	struct Node *next;
 	char payload[PAYLOAD_SIZE]; 
 };
 
+void print_cpu_info(FILE* fp);
+void do_iteration(FILE* fp, int is_random);
+size_t get_random(size_t limit);
+
 
 int main() {
-	printf("Cache size testing utility.\n");
-	printf("Calculating cache size...\n");
+	printf("Performing iterations...\n");
 	
 	time_t current_time;
 	time(&current_time);
@@ -37,11 +37,11 @@ int main() {
 	print_cpu_info(fp);
 
 	
-	fprintf(fp, "Sequential iteration time:\n");
-	fprintf(fp, "Bytes-tics\n");
+	fprintf(fp, "\nSequential iteration:\n");	
+	do_iteration(fp, 0);
 
-	do_sequential_iteration(fp);
-	// do_random_iteration(fp);
+	fprintf(fp, "\nRandom iteration:\n");	
+	do_iteration(fp, 1);
 
 	fclose(fp);
 
@@ -76,43 +76,10 @@ void print_cpu_info(FILE* fp) {
 }
 
 
-void do_sequential_iteration(FILE* fp) {
+void do_iteration(FILE* fp, int is_random) {
 
-	int iteration_time = 0;
-
-	size_t node_size = sizeof(struct Node);
-	
-	size_t elements_in_1k = BOTTOM_SIZE / node_size;
-	size_t elements_in_10M = TOP_SIZE / node_size;
-
-	for(size_t size = elements_in_1k; size < elements_in_10M; size *= 2) {
-		struct Node *buff = malloc(size * sizeof(struct Node));
-		
-		for(size_t i = 0; i < size - 1; i++) {
-			buff[i].next = &buff[i+1];
-		}
-
-		buff[size - 1].next = &buff[0];
-		clock_t start, end; 
-		start = clock();
-		struct Node *node = buff;
-		for (size_t i = 0; i < ITERATIONS; i++) {
-			node = node->next;	
-		}
-		free(buff);
-
-		end = clock();
-		iteration_time = (end - start);
-
-		fprintf(fp, "%zd", node_size*size);
-		fprintf(fp, "\t%d\n", iteration_time);
-	}
-}
-
-
-void do_random_iteration(FILE* fp) {
-
-	int iteration_time = 0;
+	fprintf(fp, "Total iterations: %i\n", ITERATIONS);
+	double iteration_time = 0;
 
 	size_t node_size = sizeof(struct Node);
 	
@@ -122,24 +89,49 @@ void do_random_iteration(FILE* fp) {
 	for(size_t size = elements_in_1k; size < elements_in_10M; size *= 2) {
 		struct Node *buff = malloc(size * sizeof(struct Node));
 		
-		for(size_t i = 0; i < size - 1; i++) {
-			buff[i].next = &buff[i+1];
+		if (is_random) {
+			for(size_t i = 0; i < size - 1; i++) {
+				buff[i].next = &buff[ get_random(size) ];
+			}	
+		} else {
+			for(size_t i = 0; i < size - 1; i++) {
+				buff[i].next = &buff[i+1];
+			}	
+		}		
+		buff[size - 1].next = &buff[0];
+
+		clock_t start, end; 
+				
+		for (size_t j = 0; j < ITERATION_CYCLES; j++) {
+			start = clock();
+			struct Node *node = buff;
+
+			for (size_t i = 0; i < ITERATIONS; i++) {
+				node = node->next;	
+			}
+
+			end = clock();
+			iteration_time += (double)(end - start) / CLOCKS_PER_SEC;	
 		}
 
-		buff[size - 1].next = &buff[0];
-		clock_t start, end; 
-		start = clock();
-		struct Node *node = buff;
-		for (size_t i = 0; i < ITERATIONS; i++) {
-			node = node->next;	
-		}
+		iteration_time /= ITERATION_CYCLES;
+		
+
 		free(buff);
 
-		end = clock();
-		iteration_time = (end - start);
-
 		fprintf(fp, "%zd", node_size*size);
-		fprintf(fp, "\t%d\n", iteration_time);
+		fprintf(fp, "\t%f\n", iteration_time);
 	}
 }
 
+
+size_t get_random(size_t limit) {
+	size_t divisor = RAND_MAX / (limit+1);
+	size_t random_number;
+
+	do {
+		random_number = rand() / divisor;
+	} while (random_number > limit);
+
+	return random_number;
+}
